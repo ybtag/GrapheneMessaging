ba9832a65d9f603fabb1604c306e3dde2f1cc1e6
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-12-20 19:16:16 +0200

    switch to Gradle build system

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+android {
+    compileSdk = 35
+    buildToolsVersion = "35.0.0"
+
+    namespace = "com.android.messaging"
+
+    defaultConfig {
+        versionCode = 10001040 + 1
+        versionName = "1.0.001"
+        minSdk = 24
+        //noinspection ExpiredTargetSdkVersion
+        targetSdk = 24
+    }
+
+    buildTypes {
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+            resValue("string", "app_name", "Messaging d")
+        }
+    }
+
+    sourceSets.getByName("main") {
+        assets.srcDir("../assets")
+        manifest.srcFile("../AndroidManifest.xml")
+        java.srcDirs("../src")
+        res.srcDir("../res")
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.palette:palette:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.guava:guava:33.4.0-android")
+
+    implementation(project(":lib:platform_external_libphonenumber"))
+    implementation(project(":lib:platform_frameworks_ex:common"))
+    implementation(project(":lib:platform_frameworks_ex:framesequence"))
+    implementation(project(":lib:platform_frameworks_opt_chips"))
+    implementation(project(":lib:platform_frameworks_opt_colorpicker"))
+    implementation(project(":lib:platform_frameworks_opt_photoviewer"))
+    implementation(project(":lib:platform_frameworks_opt_vcard"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":app")
+include(":lib:platform_external_libphonenumber")
+include(":lib:platform_frameworks_ex:common")
+include(":lib:platform_frameworks_ex:framesequence")
+include(":lib:platform_frameworks_opt_chips")
+include(":lib:platform_frameworks_opt_colorpicker")
+include(":lib:platform_frameworks_opt_photoviewer")
+include(":lib:platform_frameworks_opt_vcard")
